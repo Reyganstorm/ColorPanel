a992bd334e47af801de36537621333a897796031
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2024-01-18 12:11:14 +0600

    ADD: - new colors

--- Sources/ColorPanel.swift
+++ Sources/ColorPanel.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import UIKit
+
+
+public class ColorPanel {
+    
+    public let mainColor = UIColor.blue
+    
+    public let systemColor = UIColor.red
+}
